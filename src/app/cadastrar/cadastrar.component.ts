07a27fc17694b3ac0be67f2a2e792a38238f025e
Daniel <kr.danielreis@gmail.com>
2022-02-25 10:51:03 -0300

    @DanielSantosReis [ Inserindo as funções no cadastro :rocket: ]

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario()
+  userSenha: string
+  userTipo: string
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
   }
+
+  confirmSenha(event:any) {
+    this.userSenha = event.target.value
+  }
+
+  cadastrar(){
+    this.usuario.tipo = this.userTipo
+
+    if(this.usuario.senha != this.userSenha){
+      alert('Senha incorreta!')
+    } else {
+      this.auth.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp;
+        alert('Usuário cadastrado com êxito!)')
+        this.router.navigate(['/entrar'])
+      })
+    }
+
+
+  }
+
 
 }
