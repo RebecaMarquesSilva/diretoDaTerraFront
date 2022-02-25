156d913c02d5581082936d34437795c5cf4cdce9
Luiz Alberto Melin <luizmelin34@gmail.com>
2022-02-25 09:29:32 -0300

    @LuizAlbertoMelin [adicionando as models]

---
+++ src/app/model/produto.ts
@@ -0,0 +1,13 @@
+import { Categoria } from "./categoria";
+import { Usuario } from "./usuario";
+
+export class Produto{
+  public id: number;
+  public nome: string;
+  public preco: number;
+  public disponibilidade: string;
+  public produtor: string;
+  public quantidade: number;
+  public categoria: Categoria;
+  public usuario: Usuario;
+  }
