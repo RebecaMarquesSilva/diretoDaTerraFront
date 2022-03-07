d7829ada7366da9f9ffd27047d1a50b4a89e5d33
Kaylaine <kaylainefragoso17@gmail.com>
2022-03-07 10:51:25 -0300

    @KaylaineFragoso [add post categoria]

---
+++ src/app/service/produto.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Produto } from '../model/produto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllProduto(): Observable<Produto[]>{
+    return this.http.get<Produto[]>('http://diretodaterra.herokuapp.com/produto', this.token)
+
+  }
+
+  postProduto(produto: Produto): Observable<Produto>{
+    return this.http.post<Produto>('http://diretodaterra.herokuapp.com/produto', produto, this.token)
+  }
+  
+}
